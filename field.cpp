6472ac21ef75756c696ee1775c5fba79ddb0f022
Akihiro Takai <at.sisy@gmail.com>
2018-05-11 22:13:44 +0900

    feat: 占有したパネルのスコア計算メソッドを実装

--- field.cpp
+++ field.cpp
@@ -1,6 +1,7 @@
 #include "include/types.hpp"
 #include <cmath>
 #include <limits>
+#include <algorithm>
 
 u8 Field::ac_shift_offset;
 u64 Field::field_size;
@@ -9,6 +10,14 @@ Field::Field()
         : field(field_size)
 {}
 
+/*
+ * atメソッド　フィールド情報のアクセサメソッド
+ * 引数
+ * u8 x: フィールドのx座標
+ * u8 y: フィールドのy座標
+ * 返り値
+ * (x, y)のパネル
+ */
 Panel Field::at(u8 x, u8 y)
 {
         return field.at(x + (y << this->ac_shift_offset));
@@ -18,4 +27,62 @@ FieldBuilder::FieldBuilder(u8 width, u8 height)
 {
         Field::ac_shift_offset = (u64)(std::log2(width) + 0.5);
         Field::field_size = height << Field::ac_shift_offset;
+}
+
+/*
+ * 自分のパネルが置かれている部分の合計得点を返す
+ */
+u64 Field::calc_mypanels_score()
+{
+        u64 tmp_score = 0;
+        
+        for(Panel panel : field){
+                if(panel.is_my_panel()){
+                        tmp_score += panel.get_score_value();
+                }
+        }
+
+        return tmp_score;
+}
+
+/*
+ * 敵のパネルが置かれている部分の合計得点を返す
+ */
+u64 Field::calc_enemypanels_score()
+{
+        u64 tmp_score = 0;
+        
+        for(Panel panel : field){
+                if(panel.is_enemy_panel()){
+                        tmp_score += panel.get_score_value();
+                }
+        }
+
+        return tmp_score;
+}
+
+/*
+ * 自分の合計と敵の合計の差を返す。上記２つの関数を使って差を求めるよりも高速
+ */
+u64 Field::calc_sumpanel_score()
+{
+        u64 tmp_score = 0;
+        
+        for(Panel panel : field){
+                if(panel.is_my_panel()){
+                        tmp_score += panel.get_score_value();
+                }else if(panel.is_enemy_panel()){
+                        tmp_score -= panel.get_score_value();
+                }
+        }
+
+        return tmp_score;
+}
+
+u64 Field::score()
+{
+        /*
+         * 未実装
+         */
+        return 0;
 }
--- include/types.hpp
+++ include/types.hpp
@@ -100,11 +100,23 @@ private:
         
         std::vector<Panel> field;
 
+        /*
+         * フィールド上の得点を計算するメソッド
+         */
+        // 自分のパネルが置かれている部分の合計得点
+        u64 calc_mypanels_score();
+        // 敵のパネルが置かれている部分の合計得点
+        u64 calc_enemypanels_score();
+        // 自分の合計と敵の合計の差。上記２つの関数を使って差を求めるよりも高速
+        u64 calc_sumpanel_score();
+
 public:
         Field();
 
         // フィールド情報のアクセサメソッド
         Panel at(u8 x, u8 y);
+
+        u64 score();
 };
 
 class FieldBuilder {
