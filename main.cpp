7b69d16dc8bdba6e221f6dcda2eeba8e7b1d0595
Akihiro Takai <at.sisy@gmail.com>
2018-06-08 19:26:56 +0900

    feat: QRコードフォーマットファイルのパスをコマンドライン引数で受け取るようにした
    テストデータファイルも同封

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include "include/types.hpp"
 #include "include/utility.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-        FieldBuilder builder(new QRFormatParser("/home/takai/test.dat"));
+        /*
+         * コマンドライン引数の添字1にQRへのファイルパスが含まれているとする。
+         */
+        FieldBuilder builder(new QRFormatParser(argv[1]));
 	Field mainField;	//繝｡繧､繝ｳ縺ｨ縺ｪ繧九ヵ繧｣繝ｼ繝ｫ繝峨����繧､繝ｳ繧ｹ繧ｿ繝ｳ繧ｹ
 	
 	mainField.randSetPanel();
