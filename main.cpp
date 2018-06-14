eb99621887dc85ac65ee4b8dddc2931c26810e33
Akihiro Takai <at.sisy@gmail.com>
2018-06-14 10:09:03 +0900

    test: テストコードをコメントアウト

--- main.cpp
+++ main.cpp
@@ -14,28 +14,36 @@ int main()
         builder.print_status();
         test_generate_agent_meta();
 
-        mainField.make_at(3, 3, MINE_ATTR);
-        mainField.make_at(4, 3, MINE_ATTR);
-        mainField.make_at(5, 4, MINE_ATTR);
-        mainField.make_at(4, 5, MINE_ATTR);
-        mainField.make_at(3, 5, MINE_ATTR);
-        mainField.make_at(2, 4, MINE_ATTR);
-
-        mainField.draw_status();
-        
-        std::chrono::system_clock::time_point  start, end;
-        i16 score;
-        start = std::chrono::system_clock::now();
-
-        FieldEvaluater::set_target(generate_agent_meta(MINE_ATTR));
-        score = FieldEvaluater::calc_local_area(&mainField);
-
-        end = std::chrono::system_clock::now();
-        double elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-
-        std::cout << "score -> " << (int)score << std::endl;
-        std::cout << elapsed << "us" << std::endl;
 #endif
         
         return 0;
 }
+
+        /*****
+              FieldEvaluaterテストコード
+        {
+                mainField.randSetPanel();
+                mainField.Draw();
+                mainField.make_at(3, 3, MINE_ATTR);
+                mainField.make_at(4, 3, MINE_ATTR);
+                mainField.make_at(5, 4, MINE_ATTR);
+                mainField.make_at(4, 5, MINE_ATTR);
+                mainField.make_at(3, 5, MINE_ATTR);
+                mainField.make_at(2, 4, MINE_ATTR);
+
+                mainField.draw_status();
+        
+                std::chrono::system_clock::time_point  start, end;
+                i16 score;
+                start = std::chrono::system_clock::now();
+
+                FieldEvaluater::set_target(generate_agent_meta(MINE_ATTR));
+                score = FieldEvaluater::calc_local_area(&mainField);
+
+                end = std::chrono::system_clock::now();
+                double elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
+
+                std::cout << "score -> " << (int)score << std::endl;
+                std::cout << elapsed << "us" << std::endl;
+        }
+        */
